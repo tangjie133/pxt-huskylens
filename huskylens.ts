ec8343e9351d84f59dfd67361afb608dcd3adc93
TgJe <51323906+tangjie133@users.noreply.github.com>
2019-12-10 14:21:40 +0800

    更新 pxt.json, main.blocks, huskylens.ts

---
+++ huskylens.ts
@@ -0,0 +1,15 @@
+// 在此处添加您的代码
+
+//% weight=100  color=#00A654 block="Huskylens"
+namespace huskylens{
+
+    /**
+     * 初始化哈士奇
+     */
+
+    //% block="request data"
+    export function requestData():void{
+       
+    }
+    
+}
