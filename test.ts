180df117fb8d1acbad4e041deeda47ff79568eff
TgJe <51323906+tangjie133@users.noreply.github.com>
2020-05-27 14:46:34 +0800

    添加测试文件

--- test.ts
+++ test.ts
@@ -1,1 +1,18 @@
-// 在此处测试；当此软件包作为插件使用时，将不会编译此软件包。
+
+huskylens.initI2c()
+huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_TRACKING)
+basic.forever(function () {
+    huskylens.request()
+    if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        if (huskylens.isLearned(1)) {
+            serial.writeValue("x", huskylens.readeBox(1, Content1.xCenter))
+            serial.writeValue("y", huskylens.readeBox(1, Content1.yCenter))
+            serial.writeValue("k", huskylens.readeBox(1, Content1.width))
+            serial.writeValue("h", huskylens.readeBox(1, Content1.height))
+        } else {
+            serial.writeString("-1")
+        }
+    } else {
+        serial.writeString("-1")
+    }
+})
